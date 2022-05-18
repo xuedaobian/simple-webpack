a4619c12cf851c2bd0dbbbb31641a81f7b81fde9
xuedaobian <1689359591@qq.com>
2022-05-18 14:08:20 +0800

    stage1 init

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ function component() {
   const element = document.createElement('div');
 
   // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack', 'stage1'], ' ');
   element.classList.add('hello');
   const img = new Image();
   img.src = Logo;
